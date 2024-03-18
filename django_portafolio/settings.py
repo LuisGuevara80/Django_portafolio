fd4678591978a90a7309a89f3b044face7f7568f
LuisGuevara80 <luis.guevaraj80@gmail.com>
2024-03-18 10:46:10 -0600

    Deploy-render

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'django_portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    }
 
 
 # Password validation
